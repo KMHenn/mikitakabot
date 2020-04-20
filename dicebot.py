0cebc1bba240e32f22f5b77f33652fac3ce81eca
Kaitlyn Hennessy <khennessy@prospexdigital.com>
2020-04-20 13:53:07 -0400

    Added dice roll

--- dicebot.py
+++ dicebot.py
@@ -1,17 +1,54 @@
 import discord
+from random import randint
+from boto.s3.connection import S3Connection
 
 client = discord.Client()
+tok = S3Connection(os.environ['token'])
+
+def roll(dice):
+    numDice = int(dice[0])
+    numSides = int(dice[1])
+    total = 0
+    for x in range (0, numDice):
+        r = randint(1, numSides)
+        total += r
+    return total
+
+def errorHandle():
+    return ""
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('/'):
+        if message.content.startswith('/r'):
+            args = message.content.split(" ")
+            dice = (args[1]).split("d")
+            diceRoll = roll(dice)
+            math = 0
 
-client.run('your token here')
+            if len(args) > 2:
+                op = args[2]
+                if op == '+':
+                    math = int(args[3])
+                elif op == '-':
+                    math = int(args[3]) * -1
+                else:
+                    errorHandle()
+                    return
+
+            msg = ('{0.author.mention}: ' + str(args[1]) + ' ' + str(args[2]) + ' ' + str(args[3]) + ' = ' + str(diceRoll + math)).format(message)
+            await message.channel.send(msg)
+
+
+
+client.run(tok)
