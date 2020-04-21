8815997f97895f82c88eef4bc35675927a559659
Kaitlyn Hennessy <khennessy@prospexdigital.com>
2020-04-21 11:39:10 -0400

    Test first few commands

--- dicebot.py
+++ dicebot.py
@@ -58,15 +58,30 @@ async def on_message(message):
             await message.channel.send(error + ": Invalid command")
 
     elif message.content.startswith('!'):
-        if message.content.startswith('!engage'):
-            txt = ("When you directly engage a threat, roll + Danger. On a hit, trade blows. On a 10+, pick two. On a 7-9, pick one: " + "\n" 
+        if message.content == '!engage':
+            txt = ("{0.author.mention}: " + "When you directly engage a threat, roll + Danger. On a hit, trade blows. On a 10+, pick two. On a 7-9, pick one: " + "\n" 
             + "- Resist or avoid their blows" + "\n"
             + "- Take something from them" + "\n"
             + "- Create an opprotunity for your allies" + "\n"
             + "- Impress, surprise, or frighten the opposition").format(message)
             await message.channel.send(txt)
 
+        elif message.content == "!unleash":
+            txt = ("{0.author.mention}: " + "When you unleash your powers to overcome an obstacle, reshape your environment, or extend your senses, roll + Freak. On a hit, you do it. On a 7-9, mark a condition or the GM will tell you how the effect is unstable or temporary.").format(message)
+            await message.channel.send(txt)
+        
+        elif message.content == "!comfort":
+            txt = ("{0.author.mention}: " + "When you comfort or support someone, roll + Mundane. On a hit, they hear you: they mark potential, clear a condition, or shift Labels if they open up to you. On a 10+, you can also add a Team to the pool or clear a condition yourself.").format(message)
+            await message.channel.send(txt)
 
+        elif message.content == "!pierce":
+            txt = ("{0.author.mention}: " + "When you pierce someone’s mask to see the person beneath, roll + Mundane. On a 10+, ask three. On a 7-9, ask one: " + "\n" 
+            + "- What are you really planning?" + "\n"
+            + "- What do you want me to do?" + "\n"
+            + "- What do you intend to do?" + "\n"
+            + "- How could I get your character to ____?" + "\n"
+            + "- How could I gain influence over you?").format(message)
+            await message.channel.send(txt)
 
 
 client.run(tok)
