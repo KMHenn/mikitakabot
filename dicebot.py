7ad34d3e32967e050fd98042fbaa2fe5e68ae515
Kaitlyn Hennessy <khennessy@prospexdigital.com>
2020-04-24 13:44:59 -0400

    Tweak help command

--- dicebot.py
+++ dicebot.py
@@ -144,7 +144,7 @@ async def on_message(message):
         elif message.content == "!help":
             desc = "Get description of "
             txt = ("{0.author.mention}: " + "\n" + "Command List:" + "\n" + "\n" 
-            + "**/r [#dice]d[#sides] [+ | -] [#]**: " + "\t" + "Roll [#dice] [#sides]-sided die, [+ | -] [#]." + "\n" + "\n"
+            + "**/r [#dice]d[#sides] [+ | -] [#]**: " + "\t" + "Roll [#dice] [#sides]-sided die, [ + | - | * | / ] [#]." + "\n" + "Example: /r 2d6 + 2" + "\n" + "\n"
             + "**!engage**: " + "\t" + desc + "'Directly Engage a Threat'" + "\n"
             + "**!unleash**: " + "\t" + desc + "'Unleash Your Powers'" + "\n"
             + "**!comfort**: " + "\t" + desc + "'Comfort or Support'" + "\n"
