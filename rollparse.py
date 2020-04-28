a37c6fbc462633285f682ef4cb7b6338a8ee6dad
Kaitlyn Hennessy <khennessy@prospexdigital.com>
2020-04-28 15:19:35 -0400

    Debug

--- rollparse.py
+++ rollparse.py
@@ -48,9 +48,11 @@ def getSides(input):
     intStr = ""
 
     while input[ind] not in OPS:
+        print("\t while loop")
         intStr =  intStr + input(ind)
         ind = ind + 1
     
+    print("intStr: " + intStr)
     return [int(intStr), input[ind:]]
 
 
