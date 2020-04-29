2eb7cd30280297c17903fea8bf7d100edf7ccb05
Kaitlyn Hennessy <khennessy@prospexdigital.com>
2020-04-29 12:43:23 -0400

    debug

--- rollparse.py
+++ rollparse.py
@@ -6,25 +6,18 @@ OPS = ["+", "-", "*", "/"]
 ######################################
 ##
 ######################################
-def parse(input):#input, numDice):
+def parse(input):
     try:
-        #input = (input.content)[2:].replace(" ", "")
         diceSplit = (input).split("d")
         numDice = int(diceSplit[0])
-        print("\n\nnumDice : " + str(numDice) + "\tdiceSplit[1]: " + diceSplit[1])
         workStr = diceSplit[1]
         sideParse = getSides(workStr)
-        print("exited getSides")
         numSides = sideParse[0]
         strInd = int(sideParse[1])
-        print("numSides: " + str(numSides) + "\tstrInd: " + str(strInd))
 
         totalArr = roll(numDice, numSides)
-        print("exited roll")
         total = totalArr[0]
-        print("total: " + str(total))
-        dispStr = "/r " + str(numDice) + "d" + str(numSides) + " = (" + totalArr[1] + ")" 
-        print("dispStr: " + dispStr)
+        dispStr = "/r " + str(numDice) + "d" + str(numSides) + " = (" + totalArr[1] + ")"
         curNum = ""
         curOp = ""
 
@@ -32,7 +25,6 @@ def parse(input):#input, numDice):
             for i in range(strInd, len(workStr)):
                 print("in for loop: workStr[" + str(i) + "] = " + workStr[i])
                 if workStr[i] in OPS:
-                    curOp = workStr[i]
                     if i != strInd:
                         total = math(total, int(curNum), curOp)
                         dispStr = dispStr + " " + curOp + " " + curNum
@@ -60,22 +52,17 @@ def parse(input):#input, numDice):
 ##
 ######################################
 def getSides(input):
-    print("In getSides")
     ind = 0
     intStr = ""
 
     for x in range(0, len(input)):
         if (input[x] not in OPS):
-            print("\t while loop: input[x] = " + input[x])
             intStr =  intStr + input[x]
-            print("\t intStr = " + intStr)
             ind = x
-            print("\t x = " + str(x))
         else:
             break
 
     ind = ind + 1
-    print("ind = " + str(ind) + "\tintStr = " + intStr)
     result = [int(intStr), ind]
     return result
 
@@ -86,7 +73,6 @@ def getSides(input):
 ##
 ######################################
 def math(tot, newVal, op):
-    print("In math")
     result = ""
 
     if op == '+':
@@ -105,7 +91,6 @@ def math(tot, newVal, op):
 ## Roll specified number of dice
 ######################################
 def roll(numDice, numSides):
-    print("In roll")
     total = 0
     rollStr = ""
 
